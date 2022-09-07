fd91868bd171fd9db19477d516d7ef89aa4059a9
Xufeng Gao <xugao@izar.ib0.cm.izar.local>
2022-09-07 11:01:00 +0200

    update

--- scripts/train_sc.sh
+++ scripts/train_sc.sh
@@ -1,15 +1,40 @@
-set -ex
+#!/bin/bash
+
+#SBATCH --cpus-per-task=20
+#SBATCH --job-name=sc
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --partition=gpu
+#SBATCH --gpus-per-node=2
+#SBATCH --qos=gpu_free
+#SBATCH --mem=350GB
+#SBATCH --time=12:00:00
+#SBATCH --chdir /scratch/izar/xugao/trainLogs
+#SBATCH --output=/scratch/izar/xugao/trainLogs/sc_0_%j.out
+#SBATCH --error=/scratch/izar/xugao/trainLogs/sc_0_%j.err
+
+module load gcc cuda
+echo "hello from 'hostname'"
+
+source /home/xugao/venvs/venv-for-demo/bin/activate
+
+cd /home/xugao/gitRepo/F-LSeSim
+
 python train.py  \
---dataroot /media/lyndon/c6f4bbbd-8d47-4dcb-b0db-d788fe2b25571/dataset/image_translation/horse2zebra \
---name horse2zebra \
+--dataroot /home/xugao/gitRepo/swapping-autoencoder-pytorch/datasets/nightVisionDatasets/ \
+--name night2day \
 --model sc \
---gpu_ids 1 \
+--gpu_ids 0 \
 --lambda_spatial 10 \
 --lambda_gradient 0 \
 --attn_layers 4,7,9 \
 --loss_mode cos \
 --gan_mode lsgan \
---display_port 8093 \
+--display_port 8097 \
 --direction AtoB \
---patch_size 64
-#\--learned_attn \--augment
+--patch_size 64 \
+--learned_attn \
+--augment \
+--use_norm
+
+echo "bye from 'hostname'"
